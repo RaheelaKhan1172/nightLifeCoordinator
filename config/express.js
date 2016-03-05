153de4edc60ad48e79e7c8a2d655000ceaade1d6
Raheela Khan <raheelak20@gmail.com>
2016-03-04 23:37:55 -0800

    finished setting up tech stack

---
+++ config/express.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  compress = require('compression'),
+  morgan = require('morgan');
+
+module.exports = function() {
+  var app = express();
+
+  if (process.env.NODE_ENV === 'developement') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
+  app.use(bodyParser.urlencoded({
+    extended:true
+  }));
+  app.use(bodyParser.json());
+
+  app.set('views','./app/views');
+  app.set('view engine', 'ejs');
+
+
+  return app;
+}
