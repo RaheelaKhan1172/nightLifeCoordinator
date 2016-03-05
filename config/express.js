bddd9a9e1ed2467598dc2181b92e6df5b25d2662
Raheela Khan <raheelak20@gmail.com>
2016-03-05 11:11:28 -0800

    landing page initialized

--- config/express.js
+++ config/express.js
@@ -18,7 +18,9 @@ module.exports = function() {
 
   app.set('views','./app/views');
   app.set('view engine', 'ejs');
-
+  
+  require('../app/routes/index.server.route.js')(app);
+  app.use(express.static('./public'));
 
   return app;
 }
